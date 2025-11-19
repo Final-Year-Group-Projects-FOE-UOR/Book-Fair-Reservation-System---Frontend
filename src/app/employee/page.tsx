3f455994c813c6939d3225ef896ba7f7a47b1079
sachintharx <139696727+sachintharx@users.noreply.github.com>
2025-11-19 22:37:31 +0530

    Update page.tsx

--- src/app/employee/page.tsx
+++ src/app/employee/page.tsx
@@ -1,9 +1,7 @@
-import EmployeePortal from "@/components/employee";
-import EmployeeDashboard from "@/components/employee";
+import EmployeePortal from "@/components/employee/";
 
 const EmployeeDashboardPage = () => {
   return <EmployeePortal />;
 };
 
 export default EmployeeDashboardPage;
-
