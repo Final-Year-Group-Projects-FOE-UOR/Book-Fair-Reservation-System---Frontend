a1445b5a37c9570eba9bbbe8b956ed6517ac25ff
Shashika Gurunayake <shashikagurunayake3198@gmail.com>
2025-11-25 21:31:55 +0530

    reset password section added

--- src/actions/authActions.ts
+++ src/actions/authActions.ts
@@ -40,3 +40,24 @@ export const login = async (email: string, password: string) => {
     }
   }
 };
+
+export const initialResetPassword = async (email: string, password: string) => {
+  "use server";
+  try {
+    const body = {
+      email: email,
+      newPassword: password,
+    };
+    const url = `${Base_URL}/moderator/change-password-first-time`;
+    const response = await axios.post(url, body);
+    return response.data;
+  } catch (err: any) {
+    if (err.response && err.response.data) {
+      console.log("error", err.response.data);
+      return err.response.data;
+    } else {
+      console.log("error", err);
+      return { success: false, message: "An unexpected error occurred" };
+    }
+  }
+};
